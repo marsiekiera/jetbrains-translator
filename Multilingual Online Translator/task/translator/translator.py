80d0cba161cb4b2379f18a1fa26b422bab358281
marsiekiera <hatchet17@gmail.com>
2020-12-28 13:25:31 +0100

    Readable results

--- Multilingual Online Translator/task/translator/translator.py
+++ Multilingual Online Translator/task/translator/translator.py
@@ -19,33 +19,60 @@ def site_request(lang, word):
     url = "https://context.reverso.net/translation/" + lang_url + word
     r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
     if r.status_code == 200:
-        print("200 OK")
+        print("200 OK\n")
     else:
         return exit()
     return url
 
 
-def translation(url):
-    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
-    soup = BeautifulSoup(r.content, 'html.parser')
-    words = soup.find_all("a", ["ltr", "dict"])
+def list_of_words(words):
     words_list = []
     for word in words:
         words_list.append(word.text.strip())
+    words_list = words_list[:5]
+    for word in words_list:
+        print(word)
+
+
+def print_sentences(sentences_list):
+    counter = 0
+    for sentence in sentences_list[0:10]:
+        if counter < 2:
+            print(sentence)
+            counter += 1
+        elif counter > 1:
+            print()
+            print(sentence)
+            counter = 1
+
+
+def translation(url, lang_choice):
+    r = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
+    soup = BeautifulSoup(r.content, 'html.parser')
+    words = soup.find_all("a", ["ltr", "dict"])
     sentences_list = []
     for div in soup.find_all("div", "ltr"):
         for span in div.find_all("span"):
             sentence = span.text.strip()
             if sentence:
                 sentences_list.append(sentence)
-    print("Translations")
-    print(words_list)
-    print(sentences_list)
+    print("Context examples:\n")
+    if lang_choice == "fr":
+        print("French Translations:")
+    else:
+        print("English Translations:")
+    list_of_words(words)
+    print()
+    if lang_choice == "fr":
+        print("French Examples:")
+    else:
+        print("English Examples:")
+    print_sentences(sentences_list)
 
 
 def main():
-    user_choices = user_choice()
-    translation(site_request(user_choices[0], user_choices[1]))
+    lang_choice, word_to_trans = user_choice()
+    translation(site_request(lang_choice, word_to_trans), lang_choice)
 
 
 main()
